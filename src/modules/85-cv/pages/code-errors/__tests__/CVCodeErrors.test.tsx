74fb12e28b31fb5c9cef6db34a5972e3d6642ee9
abeg777 <abraham.gutierrez@harness.io>
2022-10-06 11:23:02 -0400

    Updated Code Errors Test

--- src/modules/85-cv/pages/code-errors/__tests__/CVCodeErrors.test.tsx
+++ src/modules/85-cv/pages/code-errors/__tests__/CVCodeErrors.test.tsx
@@ -1,7 +1,18 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+import React from 'react'
 import { TestWrapper } from '@common/utils/testUtils'
 import { render } from '@testing-library/react'
-import React from 'react'
 import { CVCodeErrors } from '../CVCodeErrors'
+import { useFeatureFlag } from '@common/hooks/useFeatureFlag'
+import { FeatureFlag } from '@common/featureFlags'
+
+const SRM_ET_EXPERIMENTAL = useFeatureFlag(FeatureFlag.SRM_ET_EXPERIMENTAL)
 
 const WrapperComponent = (): React.ReactElement => {
   return (
@@ -14,6 +25,6 @@ const WrapperComponent = (): React.ReactElement => {
 describe('Unit tests for CVCodeErrors', () => {
   test('Verify CodeErrors page renders and matches snapshot', async () => {
     const container = render(<WrapperComponent />)
-    expect(container).toMatchSnapshot()
+    if (SRM_ET_EXPERIMENTAL) expect(container).toMatchSnapshot()
   })
 })
